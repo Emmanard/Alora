3c6d9a761a6302c65a6a35f020a19415f316e57b
[Emmanard] <Emmanard9@gmail.com>
2025-09-17 14:16:01 +0100

    update

--- app/index.jsx
+++ app/index.jsx
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
+import 'react-native-url-polyfill/auto'
 import {  ScrollView, Text, View , Image} from 'react-native';
 import { Redirect, router } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import {images} from '../constants';
 import CustomButton from '../components/CustomButton';
+import {useGlobalContext} from '../context/GlobalProvider'
 
 
 export default function App() {
+  const {Loading, isLoggedIn} = useGlobalContext()
+
+  if(!Loading && isLoggedIn) return <Redirect
+   href="/home"/>
   return (
   <SafeAreaView className='bg-primary h-full' >
 <ScrollView contentContainerStyle={{height:'100'}}>
